51c99fa97f5fe1cbe78d3cce2b164557ac763d6d
Ahamed Abdul Khader <ahamedabdul24@gmail.com>
2025-04-05 23:14:52 +0100

    code change

--- supportdesk/settings.py
+++ supportdesk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rnjt874&b#et#$ogk)+86jqq@^zcbk)lsaoial+i1qc#@ctsg#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://support-desk.eu-west-1.elasticbeanstalk.com',
+ALLOWED_HOSTS = ['http://support-ticket.eu-west-1.elasticbeanstalk.com',
                  '.elasticbeanstalk.com',
                  'localhost',
                  '127.0.0.1']
